27ae476638bb8c7f7388a3b53c4c4f0f90192183
unknown <radumotrescu@gmail.com>
2020-01-03 14:20:21 +0200

    added tests for Vec2

--- src/Vec2.h
+++ src/Vec2.h
@@ -8,22 +8,22 @@ namespace MathLib
 
 	struct Vec2
 	{
-		Vec2 operator+(const Vec2& rhs)
+		Vec2 operator+(const Vec2& rhs) const
 		{
 			return { rhs.data[0] + data[0], rhs.data[1] + data[1]};
 		}
 
-		Vec2 operator-(const Vec2& rhs)
+		Vec2 operator-(const Vec2& rhs) const
 		{
 			return { rhs.data[0] - data[0], rhs.data[1] - data[1] };
 		}
 		
-		Vec2 operator*(int scalar)
+		Vec2 operator*(int scalar) const
 		{
 			return { data[0] * scalar, data[1] * scalar };
 		}
 
-		bool operator==(const Vec2& rhs)
+		bool operator==(const Vec2& rhs) const
 		{
 			return rhs.data[0] == data[0] && rhs.data[1] == data[1];
 		}
--- test/VectorTesting.cpp
+++ test/VectorTesting.cpp
@@ -7,32 +7,73 @@
 
 using namespace MathLib;
 
-TEST_CASE("Vec3 is POD type")
+TEST_SUITE("Vec3 tests")
 {
-	REQUIRE(std::is_pod<Vec3>::value);
+
+	TEST_CASE("Vec3 is POD type")
+	{
+		REQUIRE(std::is_pod<Vec3>::value);
+	}
+
+
+	TEST_CASE("Vec3 equality operator works")
+	{
+		const auto lhs = Vec3{ {1., 1., 1.} };
+		const auto expected = std::array<double, 3>{ 1., 1., 1. };
+		REQUIRE_EQ(lhs.data, expected);
+	}
+
+	TEST_CASE("Vec3 addition operator works")
+	{
+		auto lhs = Vec3{ {1., 1., 1.} };
+		auto rhs = Vec3{ {1., 1., 1.} };
+		const auto result = lhs + rhs;
+		const auto expected = Vec3{ 2., 2., 2. };
+		CHECK(result == expected);
+	}
+
+	TEST_CASE("Vec3 subtraction operator works")
+	{
+		auto lhs = Vec3{ {1., 1., 1.} };
+		auto rhs = Vec3{ {1., 1., 1.} };
+		const auto result = lhs - rhs;
+		const auto expected = Vec3{ 0., 0., 0. };
+		CHECK(result == expected);
+	}
 }
 
-TEST_CASE("Vec3 equality operator works")
-{
-	const auto lhs = Vec3{ {1., 1., 1.} };
-	const auto expected = std::array<double, 3>{ 1., 1., 1. };
-	REQUIRE_EQ(lhs.data, expected);
-}
 
-TEST_CASE("Vec3 addition operator works")
+TEST_SUITE("Vec2 tests")
 {
-	auto lhs = Vec3{ {1., 1., 1.} };
-	auto rhs = Vec3{ {1., 1., 1.} };
-	const auto result = lhs + rhs;
-	const auto expected = Vec3{ 2., 2., 2. };
-	CHECK(result == expected);
-}
 
-TEST_CASE("Vec3 subtraction operator works")
-{
-	auto lhs = Vec3{ {1., 1., 1.} };
-	auto rhs = Vec3{ {1., 1., 1.} };
-	const auto result = lhs - rhs;
-	const auto expected = Vec3{ 0., 0., 0. };
-	CHECK(result == expected);
+	TEST_CASE("Vec2 is POD type")
+	{
+		REQUIRE(std::is_pod<Vec2>::value);
+	}
+
+
+	TEST_CASE("Vec2 equality operator works")
+	{
+		const auto lhs = Vec2{ {1., 1.} };
+		const auto expected = std::array<double, 2>{ 1., 1. };
+		REQUIRE_EQ(lhs.data, expected);
+	}
+
+	TEST_CASE("Vec2 addition operator works")
+	{
+		auto lhs = Vec2{ {1., 1. } };
+		auto rhs = Vec2{ {1., 1. } };
+		const auto result = lhs + rhs;
+		const auto expected = Vec2{ 2., 2. };
+		CHECK(result == expected);
+	}
+
+	TEST_CASE("Vec2 subtraction operator works")
+	{
+		auto lhs = Vec2{ {1., 1. } };
+		auto rhs = Vec2{ {1., 1. } };
+		const auto result = lhs - rhs;
+		const auto expected = Vec2{ 0., 0. };
+		CHECK(result == expected);
+	}
 }
