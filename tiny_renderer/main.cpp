e4d7d01464d06fc6556832aa92afacdae55ad53f
unknown <radumotrescu@gmail.com>
2020-01-12 13:30:10 +0200

    added posibility to read from OBJ file + african head example

--- tiny_renderer/main.cpp
+++ tiny_renderer/main.cpp
@@ -1,18 +1,18 @@
 #include "tgaimage.h"
+#include "model.h"
+
+using namespace MathLib;
+
 namespace
 {
 	const TGAColor white = TGAColor(255, 255, 255, 255);
 	const TGAColor red = TGAColor(255, 0, 0, 255);
-
+	const int width = 800;
+	const int height = 800;
 }
 
 void line(int x0, int y0, int x1, int y1, TGAImage& image, const TGAColor& color)
 {
-	if (x0 > x1)
-	{
-		std::swap(x0, x1);
-	}
-
 	bool steep = false;
 	if (std::abs(x0 - x1) < std::abs(y0 - y1))
 	{
@@ -21,12 +21,19 @@ void line(int x0, int y0, int x1, int y1, TGAImage& image, const TGAColor& color
 		steep = true;
 	}
 
+	if (x0 > x1)
+	{
+		std::swap(x0, x1);
+		std::swap(y0, y1);
+	}
+
 	int dx = x1 - x0;
 	int dy = y1 - y0;
-	int derror = std::abs(dy)*2;
+	int derror = std::abs(dy) * 2;
 	int error = 0;
 
 	int y = y0;
+	int yIncr = y1 > y0 ? 1 : -1;
 	for (auto x = x0; x <= x1; x++)
 	{
 		if (steep)
@@ -36,21 +43,46 @@ void line(int x0, int y0, int x1, int y1, TGAImage& image, const TGAColor& color
 		error += derror;
 		if (error > dx)
 		{
-			y += (y1 > y0 ? 1 : -1);
-			error -= dx*2;
+			y += yIncr;
+			error -= dx * 2;
 		}
 	}
 }
 
 int main()
 {
-	TGAImage image(100, 100, TGAImage::RGB);
-	for (auto i = 0; i < 1000000; i++) 
+	TGAImage image(width, height, TGAImage::RGB);
+	//line(13, 20, 80, 40, image, white);
+	//line(20, 13, 40, 80, image, red);
+	//line(80, 40, 13, 20, image, red);
+
+
+	auto model = Model("obj/african_head.obj");
+	for (auto i = 0; i < model.nfaces(); i++)
 	{
-		line(13, 20, 80, 40, image, white);
-		line(20, 13, 40, 80, image, red);
-		line(80, 40, 13, 20, image, red);
+		auto face = model.face(i);
+		for (auto j = 0; j < 3; j++)
+		{
+			Vec3f v0 = model.vert(face[j]);
+			Vec3f v1 = model.vert(face[(j + 1) % 3]);
+
+			/* 
+			we do the (coord + 1) / (size / 2.) thing because the file
+			uses coordinates from [-1, +1], thus the need
+			to get rid of the negative sign
+			so the coordinates would match [0, 2],
+			thus the need to divide by the size/2
+			so the coordinates would match [0, 1]
+			*/
+			int x0 = static_cast<int>((v0.m_data[0] + 1.)*width / 2.);
+			int y0 = static_cast<int>((v0.m_data[1] + 1.)*height / 2.);
+
+			int x1 = static_cast<int>((v1.m_data[0] + 1.)*width / 2.);
+			int y1 = static_cast<int>((v1.m_data[1] + 1.)*height / 2.);
+			line(x0, y0, x1, y1, image, white);
+		}
 	}
+
 	image.flip_vertically(); // I want to have the origin at the left bottom corner of the image
 	image.write_tga_file("output.tga");
 	return 0;
---
+++ tiny_renderer/model.h
@@ -0,0 +1,20 @@
+#ifndef __MODEL_H__
+#define __MODEL_H__
+
+#include <vector>
+#include <VecN.h>
+
+class Model {
+private:
+	std::vector<MathLib::Vec3f> verts_;
+	std::vector<std::vector<int> > faces_;
+public:
+	Model(const char *filename);
+	~Model();
+	int nverts();
+	int nfaces();
+	MathLib::Vec3f vert(int i);
+	std::vector<int> face(int idx);
+};
+
+#endif //__MODEL_H__
