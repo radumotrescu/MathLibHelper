3b095860d2d8f5ebf92a7f09533a7c96f5c645d6
unknown <radumotrescu@gmail.com>
2020-01-26 18:40:27 +0200

    finished back face culling, zBuffer

--- tiny_renderer/main.cpp
+++ tiny_renderer/main.cpp
@@ -2,6 +2,7 @@
 #include "model.h"
 #include "../test/TestUtils.h"
 #include <iostream>
+#include <limits>
 
 using namespace MathLib;
 
@@ -10,89 +11,8 @@ namespace
 	const TGAColor white = TGAColor(255, 255, 255, 255);
 	const TGAColor red = TGAColor(255, 0, 0, 255);
 	const TGAColor green = TGAColor(0, 255, 0, 255);
-}
-
-void line(int x0, int y0, int x1, int y1, TGAImage& image, const TGAColor& color);
-void triangle(Vec2i p1, Vec2i p2, Vec2i p3, TGAImage& image, const TGAColor& color);
-
-void bmw()
-{
-	const int width = 3000;
-	const int height = 1000;
-	TGAImage image(width, height, TGAImage::RGB);
-
-	auto model = Model("obj/bmw27_gpu.obj");
-	for (auto i = 0; i < model.nfaces(); i++)
-	{
-		// each face has 3 lines
-		auto face = model.face(i);
-		if (face.empty())
-			continue;
-
-		for (auto j = 0; j < 3; j++)
-		{
-			// each line has 3 vertices 
-			// what this code does is goes and fetches every line from the face
-			// so line 1 and line 2, line 2 and line 3, line 3 and line 1 (0 based)
-			Vec3f v0 = model.vert(face[j]);
-			Vec3f v1 = model.vert(face[(j + 1) % 3]);
-
-			/*
-			we do the (coord + 1) / (size / 2.) thing because the file
-			uses coordinates from [-1, +1], thus the need
-			to get rid of the negative sign
-			so the coordinates would match [0, 2],
-			thus the need to divide by the size/2
-			so the coordinates would match [0, 1]
-			*/
-			int x0 = static_cast<int>((v0.m_data[0] + 3.) * width);
-			int y0 = static_cast<int>((v0.m_data[1] + 1.) * width);
-
-			int x1 = static_cast<int>((v1.m_data[0] + 3.) *height);
-			int y1 = static_cast<int>((v1.m_data[1] + 1.)*height);
-		}
-	}
-
-	image.flip_vertically(); // I want to have the origin at the left bottom corner of the image
-	image.write_tga_file("output.tga");
-}
-
-void african_head()
-{
 	const int width = 800;
 	const int height = 800;
-	TGAImage image(width, height, TGAImage::RGB);
-
-	auto model = Model("obj/african_head.obj");
-	const auto light_dir = Vec3f{ 0 , 0, -1 };
-
-	auto timer = TestUtils::Timer();
-	for (auto i = 0; i < model.nfaces(); i++)
-	{
-		// each face has 3 lines
-		auto face = model.face(i);
-		if (face.empty())
-			continue;
-
-		Vec2i screenCoords[3];
-		Vec3f world_coords[3];
-		for (int j = 0; j < 3; j++)
-		{
-			world_coords[j] = model.vert(face[j]);
-			screenCoords[j] = Vec2i({ static_cast<int>((world_coords[j].X() + 1.)*width / 2.), static_cast<int>((world_coords[j].Y() + 1.)*height / 2.) });
-		}
-
-		auto n = (world_coords[2] - world_coords[0]).Cross(world_coords[1] - world_coords[0]);
-		n.Normalize();
-		auto intensity = n.Dot(light_dir);
-
-		if (intensity > 0)
-			triangle(screenCoords[0], screenCoords[1], screenCoords[2], image, TGAColor(intensity * 255, intensity * 255, intensity * 255, 255));
-	}
-	std::cout << '\n' << timer.Elapsed() << " milliseconds";
-
-	image.flip_vertically(); // I want to have the origin at the left bottom corner of the image
-	image.write_tga_file("output.tga");
 }
 
 void line(int x0, int y0, int x1, int y1, TGAImage& image, const TGAColor& color)
@@ -135,40 +55,30 @@ void line(int x0, int y0, int x1, int y1, TGAImage& image, const TGAColor& color
 
 struct BoundingBox
 {
-	int minX, maxX, minY, maxY;
+	double minX, maxX, minY, maxY;
 };
 using BB = BoundingBox;
 
-BB findBB(Vec2i p1, Vec2i p2, Vec2i p3)
+BB findBB(Vec3f p1, Vec3f p2, Vec3f p3)
 {
 	auto minMaxX = std::minmax({ p1.X(), p2.X(), p3.X() });
 	auto minMaxY = std::minmax({ p1.Y(), p2.Y(), p3.Y() });
 	auto minX = minMaxX.first;
-	auto maxX = minMaxX.second;
+	auto maxX = std::min(width - 1., minMaxX.second);
 	auto minY = minMaxY.first;
-	auto maxY = minMaxY.second;
+	auto maxY = std::min(height - 1., minMaxY.second);
 	return { minX, maxX, minY, maxY };
 }
 
-Vec3f barycentric(Vec2i p1, Vec2i p2, Vec2i p3, Vec2i P)
+Vec3f barycentric(Vec3f p1, Vec3f p2, Vec3f p3, Vec3f P)
 {
-	/*
-	Vec3f u = cross(
-	Vec3f(pts[2][0] - pts[0][0], pts[1][0] - pts[0][0], pts[0][0] - P[0]),
-	Vec3f(pts[2][1] - pts[0][1], pts[1][1] - pts[0][1], pts[0][1] - P[1]));
-	*/
-
-	auto u = Vec3f{
-		 {0. + p2.X() - p1.X(), 0. + p3.X() - p1.X(), 0. + p1.X() - P.X()
-		} }.Cross(Vec3f
-			{ 0. + p2.Y() - p1.Y(),  0. + p3.Y() - p1.Y(),  0. + p1.Y() - P.Y()
-			});
+	auto u = Vec3f{{p2.X() - p1.X(), p3.X() - p1.X(), p1.X() - P.X()}}.Cross(Vec3f{p2.Y() - p1.Y(),p3.Y() - p1.Y(), p1.Y() - P.Y()});
 	if (std::abs(u.Z()) < 1)
 		return { -1., 1., 1. };
-	return Vec3f{ {1. - (u.X() + u.Y()) / u.Z(), static_cast<double>(u.Y()) / u.Z(), static_cast<double>(u.X()) / u.Z()} };
+	return Vec3f{ {1. - (u.X() + u.Y()) / u.Z(), u.Y() / u.Z(), u.X() / u.Z()} };
 }
 
-void triangle(Vec2i p1, Vec2i p2, Vec2i p3, TGAImage& image, const TGAColor& color)
+void triangle(Vec3f p1, Vec3f p2, Vec3f p3, std::vector<double>& zBuffer, TGAImage& image, const TGAColor& color)
 {
 	auto bb = findBB(p1, p2, p3);
 
@@ -179,7 +89,12 @@ void triangle(Vec2i p1, Vec2i p2, Vec2i p3, TGAImage& image, const TGAColor& col
 			auto bc = barycentric(p1, p2, p3, { x, y });
 			if (bc.X() < 0 || bc.Y() < 0 || bc.Z() < 0)
 				continue;
-			image.set(x, y, color);
+			auto z = p1.Z() * bc.X() + p2.Z() * bc.Y() + p3.Z() * bc.Z();
+			if (z >= zBuffer[static_cast<int>(x + y * width)])
+			{
+				zBuffer[static_cast<int>(x + y * width)] = z;
+				image.set(x, y, color);
+			}
 		}
 	}
 }
@@ -188,13 +103,50 @@ void triangle_tests()
 {
 	TGAImage image(300, 300, TGAImage::RGB);
 
-	auto t1 = std::vector<Vec2i>{ Vec2i{0, 2},   Vec2i{0, 5},  Vec2i{10, 0} };
+	auto t1 = std::vector<Vec2i>{ Vec2i{50, 10},   Vec2i{30, 50},  Vec2i{10, 100} };
 	auto t2 = std::vector<Vec2i>{ Vec2i{180, 50},  Vec2i{150, 1},   Vec2i{70, 180} };
 	auto t3 = std::vector<Vec2i>{ Vec2i{180, 150}, Vec2i{120, 160}, Vec2i{130, 180} };
 
-	triangle(t1[0], t1[1], t1[2], image, white);
-	triangle(t2[0], t2[1], t2[2], image, red);
-	triangle(t3[0], t3[1], t3[2], image, green);
+	//triangle(t1[0], t1[1], t1[2], image, white);
+	//triangle(t2[0], t2[1], t2[2], image, red);
+	//triangle(t3[0], t3[1], t3[2], image, green);
+
+	image.flip_vertically(); // I want to have the origin at the left bottom corner of the image
+	image.write_tga_file("output.tga");
+}
+
+void african_head()
+{
+	TGAImage image(width, height, TGAImage::RGB);
+
+	auto model = Model("obj/african_head.obj");
+	const auto light_dir = Vec3f{ 0 , 0, -1 };
+	auto zBuffer = std::vector<double>(width*height, std::numeric_limits<double>::lowest());
+
+	auto timer = TestUtils::Timer();
+	for (auto i = 0; i < model.nfaces(); i++)
+	{
+		// each face has 3 lines
+		auto face = model.face(i);
+		if (face.empty())
+			continue;
+
+		Vec3f screenCoords[3];
+		Vec3f worldCoords[3];
+		for (int j = 0; j < 3; j++)
+		{
+			worldCoords[j] = model.vert(face[j]);
+			screenCoords[j] = Vec3f{ std::round((worldCoords[j].X() + 1.)*width / 2.), std::round((worldCoords[j].Y() + 1.)*height / 2.), worldCoords[j].Z() };
+		}
+
+		auto n = (worldCoords[2] - worldCoords[0]).Cross(worldCoords[1] - worldCoords[0]);
+		n.Normalize();
+		auto intensity = n.Dot(light_dir);
+
+		if (intensity > 0)
+			triangle(screenCoords[0], screenCoords[1], screenCoords[2], zBuffer, image, TGAColor(intensity * 255, intensity * 255, intensity * 255, 255));
+	}
+	std::cout << '\n' << timer.Elapsed() << " milliseconds";
 
 	image.flip_vertically(); // I want to have the origin at the left bottom corner of the image
 	image.write_tga_file("output.tga");
@@ -204,6 +156,6 @@ int main()
 {
 	triangle_tests();
 	//bmw();
-	//african_head();
+	african_head();
 	return 0;
 }
