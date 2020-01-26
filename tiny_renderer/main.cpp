e7d4f18e8ee608412eea6091a6ec8af9b5c50980
unknown <radumotrescu@gmail.com>
2020-01-26 13:42:16 +0200

    second working version of triangle drawing

--- tiny_renderer/main.cpp
+++ tiny_renderer/main.cpp
@@ -8,8 +8,8 @@ namespace
 	const TGAColor white = TGAColor(255, 255, 255, 255);
 	const TGAColor red = TGAColor(255, 0, 0, 255);
 	const TGAColor green = TGAColor(0, 255, 0, 255);
-	const int width = 250;
-	const int height = 250;
+	const int width = 20;
+	const int height = 20;
 }
 
 void line(int x0, int y0, int x1, int y1, TGAImage& image, const TGAColor& color);
@@ -157,8 +157,12 @@ Vec3f barycentric(Vec2i p1, Vec2i p2, Vec2i p3, Vec2i P)
 	Vec3f(pts[2][1] - pts[0][1], pts[1][1] - pts[0][1], pts[0][1] - P[1]));
 	*/
 
-	auto u = Vec3f{ {0.+ p3.X() - p1.X(), 0.+ p2.X() - p1.X(), 0.+ p1.X() - P.X()} }.Cross(Vec3f{ 0.+ p3.Y() - p1.Y(),  0.+p2.Y() - p1.Y(),  0.+p1.Y() - P.Y() });
-	if (u.Z() < 1)
+	auto u = Vec3f{ 
+		 {0.+ p2.X() - p1.X(), 0.+ p3.X() - p1.X(), 0.+ p1.X() - P.X()
+		}}.Cross(Vec3f
+		{ 0.+ p2.Y() - p1.Y(),  0.+p3.Y() - p1.Y(),  0.+p1.Y() - P.Y()
+		});
+	if ( std::abs(u.Z()) < 1)
 		return { -1., 1., 1. };
 	return Vec3f{ {1. - (u.X() + u.Y()) / u.Z(), static_cast<double>(u.Y()) / u.Z(), static_cast<double>(u.X()) / u.Z()} };
 }
@@ -167,14 +171,9 @@ void triangle(Vec2i p1, Vec2i p2, Vec2i p3, TGAImage& image, const TGAColor& col
 {
 	auto bb = findBB(p1, p2, p3);
 
-	line(bb.minX, bb.minY, bb.maxX, bb.minY, image, white);
-	line(bb.minX, bb.minY, bb.minX, bb.maxY, image, white);
-	line(bb.maxX, bb.minY, bb.maxX, bb.maxY, image, white);
-	line(bb.minX, bb.maxY, bb.maxX, bb.maxY, image, white);
-
-	for (auto x = bb.minX; x < bb.maxX; x++)
+	for (auto x = bb.minX; x <= bb.maxX; x++)
 	{
-		for (auto y = bb.minY; y < bb.maxY; y++)
+		for (auto y = bb.minY; y <= bb.maxY; y++)
 		{
 			auto bc = barycentric(p1, p2, p3, { x, y });
 			if (bc.X() < 0 || bc.Y() < 0 || bc.Z() < 0)
@@ -186,9 +185,9 @@ void triangle(Vec2i p1, Vec2i p2, Vec2i p3, TGAImage& image, const TGAColor& col
 
 void triangle_tests()
 {
-	TGAImage image(width, height, TGAImage::RGB);
+	TGAImage image(11, 11, TGAImage::RGB);
 
-	auto t1 = std::vector<Vec2i>{ Vec2i{10, 70},   Vec2i{50, 160},  Vec2i{70, 80} };
+	auto t1 = std::vector<Vec2i>{ Vec2i{0, 10},   Vec2i{0, 0},  Vec2i{10, 0} };
 	//auto t2 = std::vector<Vec2i>{ Vec2i{180, 50},  Vec2i{150, 1},   Vec2i{70, 180} };
 	//auto t3 = std::vector<Vec2i>{ Vec2i{180, 150}, Vec2i{120, 160}, Vec2i{130, 180} };
 
